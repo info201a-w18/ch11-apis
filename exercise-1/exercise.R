06a8c50593450638549b40d3fee2032d1c03213e
Joel Ross <joelross@uw.edu>
2018-02-01 08:21:52 -0800

    Add exercise solutions

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,47 +2,53 @@
 
 # Load the httr and jsonlite libraries for accessing data
 # You can also load `dplyr` if you wish to use it
-
+library("httr")
+library("jsonlite")
+library("dplyr")
 
 # Create a variable for the API's base URI (https://api.github.com)
-
+base.uri <- "https://api.github.com"
 
 # Under the "Repositories" category, find the endpoint that will list repos in 
 # an organization
 # Create a variable `resource` that represents the endpoint for the course
 # organization (you can use `paste0()` to construct this, or enter it manually)
+resource <- "/orgs/info201/repos"  # general example
 
-
-# Send a GET request to this endpoint (the base.uri followed by the resource) 
+# Send a GET request to this endpoint (the base.uri followed by the resource)
 # and extract the response body
-
+response <- GET(paste0(base.uri, resource))
+body <- content(response,"text")
 
 # Convert the body from JSON into a data frame
-
+info.repos <- fromJSON(body)
 
 # How many (public) repositories does the organization have?
-
+print(nrow(info.repos))
 
 
 # Use a "Search" endpoint to search for repositories about "visualization" whose
 # language includes "R"
 # Reassign the `resource` variable to refer to the appropriate resource.
-
+resource <- '/search/repositories'
 
 # You will need to specify some query parameters. Create a `query.params` list 
 # variable that specifies an appropriate key and value for the search term and
 # the language
-
+query.params <- list(q = "visualization", language = "R")
 
 # Send a GET request to this endpoint--including your params list as the `query`.
 # Extract the response body and convert it from JSON.
-
+response <- GET(paste0(base.uri, resource), query = query.params)
+body <- content(response, "text")
+results <- fromJSON(body)
 
 # How many search repos did your search find? (Hint: check the list names)
-
+print(results$total_count)
 
 # What are the full names of the top 5?
-
+vis.repo.names <- results$items$full_name[1:5]
+print(vis.repo.names)
 
 
 # Use the API to determine the number of people following Hadly Wickham 
@@ -52,4 +58,8 @@
 # and use it to get a list of contributors to the `tidyverse/dplyr` repository.
 # Who were the top 10 contributor in terms of number of total commits?
 # NOTE: This will be a really big response with lots of data!
-
+resource <- '/repos/tidyverse/dplyr/stats/contributors'
+response <- GET(paste0(base.uri, resource))
+result <- flatten(fromJSON(content(response, "text")))
+contributors <- result %>% select(author.login, total) %>% arrange(-total)
+print(contributors[1:10])
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,10 +6,10 @@ library("jsonlite")
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.R")
 
 # Create a variable `movie.name` that is the name of a movie of your choice.
-
+movie.name <- "Star Wars"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -19,26 +19,38 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie.name` variable as the search query!
-
+base.uri <- "https://api.nytimes.com/svc/movies/v2"
+resource <- "/reviews/search.json"
+query.params <- list("api-key" = nyt.apikey, query = movie.name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(paste0(base.uri,resource), query = query.params)
+body <- fromJSON(content(response, "text"))
 
 # What kind of data structure did this produce? A data frame? A list?
-
+class(body)  # list
+is.data.frame(body)  # FALSE
+is.list(body)  # TRUE
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+names(body)
+names(body$results)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(body$results)
+View(reviews)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+first.review <- reviews[1,]
+headline <- first.review$headline
+summary <- first.review$summary_short
+link <- first.review$link.url
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+info <- list(headline=headline, summary=summary, link=link)
+print(info)
